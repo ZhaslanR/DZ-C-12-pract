7986ef6ae3a6e3b3a8003b28a89763f542f89344
ZhaslanR <zhaslan_12@bk.ru>
2017-12-19 17:34:58 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass test = new TestClass();
+            test.PropertyChanged += EventReader;
+            test.Number = 8;
+
+            TestClass test1 = new TestClass();
+            test.PropertyChanged += EventReader;
+            test.Number = 10;
+
+            Console.ReadLine();
+        }
+
+        static void EventReader(object sender,PropertyEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
---
+++ ConsoleApp26/TEstClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+    public class TestClass : IPropertychanged
+    {
+        public event PropertyEventHandler PropertyChanged;
+        PropertyEventArgs args = new PropertyEventArgs();
+
+        public int number;
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                args.Message = String.Format("Значение измениловь на {0}.", value);
+                EventOccured();
+            }
+        }
+
+        private void EventOccured()
+        {
+            PropertyChanged?.Invoke(this, args);
+        }
+    }
+}
